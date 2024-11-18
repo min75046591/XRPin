35907a8a5f041e35e9cc702b3072347dc4620612
jumsun <pjsw8361@gmail.com>
2024-11-18 10:02:01 +0900

    feat: 인터페이스 UI 개선

---
+++ shyeon/Arnotes/Assets/ARNotes/Script/VideoCommander.cs
@@ -0,0 +1,19 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class VideoCommander : MenuCommander
+//{
+//    public InterfaceToggle toggle;
+//    public PinchDectector pinchDectector;
+
+//    public override void Command(string commandParam)
+//    {
+//        pinc.SetActive(true);
+//    }
+
+//    public void SetCurrentPin(Pin pin)
+//    {
+//        memoSaver.SetCurrentPin(pin);
+//    }
+//}
